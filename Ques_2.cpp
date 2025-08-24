acdf95d847d7aa4a032d2417f58acad80287ed5c
Prateekyadav077 <pyadav_be24@thapar.edu>
2025-08-25 00:01:21 +0530

    Update Ques_2.cpp

--- Ques_2.cpp
+++ Ques_2.cpp
@@ -16,7 +16,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
           if (arr[i] == arr[j]) {
-                // Shift elements to left
+                
             for (int k = j; k < n - 1; k++) {
               arr[k] = arr[k + 1];
              }
